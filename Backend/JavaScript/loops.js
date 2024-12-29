554ea43acd6090c51672a47099d21c3c573e220d
Alok Gupta <154364202+alokg-812@users.noreply.github.com>
2024-12-29 11:07:08 +0530

    Create loops.js

---
+++ Backend/JavaScript/loops.js
@@ -0,0 +1,11 @@
+// for loop-> mostly when termination condition is known
+for(var i =0; i<10;i++){
+    console.log(i+1);
+}
+
+// while loop-> mostly when termination condition is unknown
+var j = 0;
+while(j<10){
+    console.log(i+1);
+    j++;
+}
