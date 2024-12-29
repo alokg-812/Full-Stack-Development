b792feb8c5ad0eb7ca482d0374ea7edd4feea820
Alok Gupta <154364202+alokg-812@users.noreply.github.com>
2024-12-29 11:07:35 +0530

    code

--- Backend/JavaScript/loops.js
+++ Backend/JavaScript/loops.js
@@ -9,3 +9,20 @@ while(j<10){
     console.log(i+1);
     j++;
 }
+
+//break and continue
+//break-> used to terminate the loop
+for(var i =0; i<10;i++){
+    if(i==5){
+        break; // terminated after i == 5
+    }
+    console.log(i);
+}
+
+//continue-> used to skip the current iteration and move to the next iteration
+for(var i =0; i<10;i++){
+    if(i==5){
+        continue; // skipped at i == 5 only
+    }
+    console.log(i);
+}
