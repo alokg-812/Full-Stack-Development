5aac0f842b253e68eeb13553e179f24040729354
Alok Gupta <154364202+alokg-812@users.noreply.github.com>
2024-12-29 11:09:45 +0530

    code

---
+++ Backend/JavaScript/dataTypes.js
@@ -0,0 +1,11 @@
+/** 
+ * Primitive:
+ * 1. String : sequence of characters, defined with either '' or with ""
+ * 2. Number : any numerical value
+ * 3. bigint : Big integer numbers
+ * 4. NaN :  Not a Number but of number datatype
+ * 5. Null : itself is of null datatype but javascript has a bug of showing this as an object but they dont want to rectify this bug
+ * 6. Undefined : variable is declared but not assigned any value
+ * 7. Boolean : true or false
+ * 8. Symbot : symbol(), constructor, symbol object, keys in objects because they are unique
+ */
