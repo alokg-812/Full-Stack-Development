0cfeffce14fc313fb0e0a5b4d87934933fc13660
Alok Gupta <154364202+alokg-812@users.noreply.github.com>
2024-12-30 11:03:35 +0530

    code

---
+++ Backend/JavaScript/var_let_const.js
@@ -0,0 +1,18 @@
+/**
+ * var:
+ * 1. var is used to define a variable
+ * 2. var is function scoped, meaning it is available in the entire function, but no block scope
+ * 3. var is hoisted, meaning it is moved to the top of the function
+ */
+
+// function f1(){
+//     var i = 5;
+//     console.log(i); 
+// }
+// console.log(i);
+// f1();
+
+{
+    var j = 5; //no block scope
+}
+console.log(j);
