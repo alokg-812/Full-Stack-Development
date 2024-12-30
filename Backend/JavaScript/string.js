6df667f98452b58115cee84d69e005d23b73fad2
Alok Gupta <154364202+alokg-812@users.noreply.github.com>
2024-12-30 11:22:48 +0530

    code

---
+++ Backend/JavaScript/string.js
@@ -0,0 +1,11 @@
+/**
+ * String:
+ * 1. Sequence of characters
+ * 2. Immutable
+ */
+
+let name = 'Alok'
+console.log(name) // Alok
+
+// indexing in the string is 0 based
+console.log(name[0]) // A
