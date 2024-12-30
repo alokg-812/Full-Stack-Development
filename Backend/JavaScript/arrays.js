90f8fac0380c83cce9aa3c0a030d84d1e6f991f5
Alok Gupta <154364202+alokg-812@users.noreply.github.com>
2024-12-30 12:20:11 +0530

    code

---
+++ Backend/JavaScript/arrays.js
@@ -0,0 +1,15 @@
+/**
+ * Arrays:
+ * 1. array is an ordered data structure
+ * 2. order in array is maintained by their indices
+ * 3. array is a collection of elements of the same or different data types
+ * 4. array is a collection of elements which are stored in contiguous memory locations
+ * 5. array in javascript is not any seperate data type, its type falls in the category of objects 
+ * 
+ */
+
+arr1 = [1,2,3]
+console.log(typeof arr1); //will give object and not array
+
+arr2 = new Array(1,2) //another way of array creation
+console.log(arr2);
