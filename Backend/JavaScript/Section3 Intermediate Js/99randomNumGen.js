0cc1798382c63e6bf72073fa01ebca9a1dfb9e90
Alok Gupta <154364202+alokg-812@users.noreply.github.com>
2025-01-12 10:12:36 +0530

    code

---
+++ Backend/JavaScript/Section3 Intermediate Js/99randomNumGen.js
@@ -0,0 +1,10 @@
+var n = Math.random();
+console.log(n);//will generate floating numbers b/w 0(inclusive) and 1(exclusive)
+
+// code snippet to generate randomly between 1 to 100
+var n = Math.random();
+console.log(Math.floor(n*100));
+
+// code snippet to generate res on dice when rolled:
+var n = Math.random();
+console.log(Math.floor(n*6) + 1);
