5821e0aff5d2168eff10a083197c8fc7c1778394
Alok Gupta <154364202+alokg-812@users.noreply.github.com>
2025-05-15 15:31:43 +0530

    Add files via upload

--- FashionStore/javascript/day11_string_methods/slice.js
+++ FashionStore/javascript/day11_string_methods/slice.js
@@ -1,1 +1,11 @@
-
+let str = 'sachinrameshtendulkar'
+console.log(str.slice(6, -5));                  //rameshtend
+console.log(str.slice(6, 16));                  //rameshtend
+console.log(str.slice(NaN, NaN));               //
+console.log(str.slice(null, null)); 		//
+console.log(str.slice(undefined, undefined));   //sachinrameshtendulkar
+console.log(str.slice(true, true));		//
+console.log(str.slice(false, true));		//s
+console.log(str.slice(0, true));		//s
+console.log(str.slice(false, null));		//
+console.log(str.slice(false, undefined));	//sachinrameshtendulkar
