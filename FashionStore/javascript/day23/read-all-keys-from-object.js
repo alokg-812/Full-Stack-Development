8fc57ad8ea6a7480158e211067a44199aec6b372
alokg-812 <alokji812@gmail.com>
2025-06-11 13:41:06 +0530

    day23 started

---
+++ FashionStore/javascript/day23/read-all-keys-from-object.js
@@ -0,0 +1,37 @@
+const users = {
+    'name' : 'sachin',
+    'age' : 52,
+    'isMarried': true
+}
+
+console.log(Object.keys(users))
+
+Object.keys(users).forEach((key)=>{
+    console.log(`${key} : ${users[key]}`)
+})
+
+console.log(Object.keys(users).length)
+
+Object.keys(users).forEach((key)=>{
+    console.log(`${key} : ${users[key]}`)
+})
+
+
+Object.keys(users).forEach((key)=>{
+    console.log(`${key} : ${users[key]} --type--> ${typeof(users[key])}`)
+})
+
+delete users.isMarried;
+
+Object.keys(users).forEach((key)=>{
+    console.log(`${key} : ${users[key]} --type--> ${typeof(users[key])}`)
+})
+
+
+
+
+
+
+
+const [username, userage, usermarried] = Object.keys(users);
+console.log(username, userage, usermarried)
