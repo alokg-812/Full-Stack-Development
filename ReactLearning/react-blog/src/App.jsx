b3cc3209689e755c9676300b18701737ec3318ec
alokg-812 <alokji812@gmail.com>
2025-07-25 11:01:46 +0530

    code

--- ReactLearning/react-blog/src/App.jsx
+++ ReactLearning/react-blog/src/App.jsx
@@ -1,17 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <h1>Hello world</h1>
-      <h1>Hello react 19</h1>
-    </>
-  )
+import Header from "./Header";
+function Apple(){
+  return <div>
+  <Header />
+      <h1>Hello India</h1>
+  </div>
 }
 
-export default App
+export default Apple
