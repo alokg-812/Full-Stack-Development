ee8dc96dfb9ccd91e1ea3e1c5e283f501302c125
alokg-812 <alokji812@gmail.com>
2025-09-01 23:58:22 +0530

    learning react

---
+++ ReactLearning/react-blog/src/Components/JSXExercise.jsx
@@ -0,0 +1,13 @@
+import ironMan from "../assets/images/ironMan.png";
+
+function Exercise(){
+    return (
+        <div>
+            <h1>Alok Gupta</h1>
+            <img src="{ironMan}" alt="" />
+            <h2>image above</h2>
+        </div>
+    )
+}
+
+export default Exercise;
