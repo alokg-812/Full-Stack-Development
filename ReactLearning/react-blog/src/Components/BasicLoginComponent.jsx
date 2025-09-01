f0588a8462df2b8bb1f79d82f09c7d62d470d495
alokg-812 <alokji812@gmail.com>
2025-09-01 23:56:07 +0530

    learning react

---
+++ ReactLearning/react-blog/src/Components/BasicLoginComponent.jsx
@@ -0,0 +1,20 @@
+function BasicLoginComponent(){
+    return (
+        <div>
+            <h3>Login here:</h3>
+            <dl>
+                <dt>Enter your email here:</dt>
+                <dd>
+                    <input type="text" name="" id="" />
+                </dd>
+                <dt>Enter password:</dt>
+                <dd>
+                    <input type="password" name="" id="" />
+                </dd>
+                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Click Me</button>
+            </dl>
+        </div>
+    )
+}
+
+export default BasicLoginComponent;
