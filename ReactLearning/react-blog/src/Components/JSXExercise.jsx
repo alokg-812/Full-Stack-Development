32e08c8c15f564636928ed0de9c9c40e88a11596
alokg-812 <alokji812@gmail.com>
2025-09-02 22:56:18 +0530

    learning phase

--- ReactLearning/react-blog/src/Components/JSXExercise.jsx
+++ ReactLearning/react-blog/src/Components/JSXExercise.jsx
@@ -1,11 +1,20 @@
 import ironMan from "../assets/images/ironMan.png";
 
 function Exercise(){
+    function callFunc(){
+        alert('Function called!');
+    }
     return (
         <div>
             <h1>Alok Gupta</h1>
-            <img src="{ironMan}" alt="" />
-            <h2>image above</h2>
+            <img src={ironMan} alt="Iron Man photo" height={300} width={200}/>
+            <ul>
+                <li>Invent a traffic light</li>
+                <li>Rehearse a Movie scene</li>
+                <li>Improve the spectrum technology</li>
+            </ul>
+            <button onClick={callFunc}>Click me</button>
+            
         </div>
     )
 }
