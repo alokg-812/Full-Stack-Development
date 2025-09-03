891a36594e65212f343d111920f9cff2eb0dc7ab
alokg-812 <alokji812@gmail.com>
2025-09-03 23:37:31 +0530

    learning phase

--- ReactLearning/react-blog/src/main.jsx
+++ ReactLearning/react-blog/src/main.jsx
@@ -5,12 +5,16 @@ import App from './App.jsx'
 import BasicLoginComponent from './Components/BasicLoginComponent.jsx';
 import JSX from './Components/JavascriptHTML.jsx';
 import Exercise from './Components/JSXExercise.jsx';
+import JSXWithCurlyBraces from './Components/JSXWithCurlyBraces.jsx';
+import ClickEvent from './Components/ClickEvent.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-    <BasicLoginComponent />
-    <JSX/>
-    <Exercise/>
+    {/* <BasicLoginComponent /> */}
+    {/* <JSX/> */}
+    {/* <Exercise/> */}
+    <JSXWithCurlyBraces/>
+    <ClickEvent />
   </StrictMode>,
 )
