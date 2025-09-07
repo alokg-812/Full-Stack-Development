42332b38cbc8a7f9ac135e80fc8cc89f7c0efe03
alokg-812 <alokji812@gmail.com>
2025-09-07 22:44:13 +0530

    learning phase

---
+++ ReactLearning/react-blog/src/Components/Props1.jsx
@@ -0,0 +1,16 @@
+import Props2 from "./Props2"
+
+function Props1({objt}){
+    return(
+        <div>
+            <h1>Using Props of JSX</h1>
+            {/* <h4>Name: {name}</h4> */}
+            {/* <h4>Age: {age}</h4> */}
+            {/* <h4>Email: {email}</h4> */}
+            <h4>Name: {objt.name}</h4>
+            <h4>Age: {objt.age}</h4>
+        </div>
+    )
+}
+
+export default Props1;
