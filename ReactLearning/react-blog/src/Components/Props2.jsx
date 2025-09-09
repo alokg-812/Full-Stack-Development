d403ea7807a2062f222633dba489f928c799b09e
alokg-812 <alokji812@gmail.com>
2025-09-09 09:51:42 +0530

    learning react

--- ReactLearning/react-blog/src/Components/Props2.jsx
+++ ReactLearning/react-blog/src/Components/Props2.jsx
@@ -1,16 +1,22 @@
 import Props1 from './Props1';
 
 function Props2(){
-    let obj={
+    let obj1={
         "name":"Alok",
         "age":20,
         "email":"alok@gmail.com",
-        "isMarried":false,
-        "salary":undefined
+        "salary":45000
+    }
+    let obj2={
+        "name":"Akanksha",
+        "age":21,
+        "email":"akanksha@gmail.com",
+        "salary":50000
     }
     return(
         <div>
-            <Props1 objt={obj}/>
+            <Props1 objt={obj1}/>
+            <Props1 objt={obj2}/>
         </div>
     )
 }
