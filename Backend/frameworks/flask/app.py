843a8260a28b89b8d214e50522c54d9454deb3bf
alokg-812 <alokji812@gmail.com>
2025-10-06 22:19:55 +0530

    learning continued

--- Backend/frameworks/flask/app.py
+++ Backend/frameworks/flask/app.py
@@ -2,10 +2,14 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+'''
 @app.route("/")
 def index():
-    if "name" in request.args:
-        name = request.args["name"]
-    else:
-        name = "alok"
+    name = request.args.get("name", "alok")
     return render_template("index.html", name=name) # this will only work when the user sends some data through the /?name="xyz"
+'''
+
+@app.route("/greet")
+def greet():
+    name = request.args.get("name", "advik")
+    return render_template("form.html", name=name)
