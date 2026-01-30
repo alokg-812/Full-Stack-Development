218a392e94e8699e1c7478ac5d3441351283eb70
alokg-812 <alokji812@gmail.com>
2026-01-30 19:36:23 +0530

    register and login function

--- Backend/frameworks/flaskUserAuthentication/app.py
+++ Backend/frameworks/flaskUserAuthentication/app.py
@@ -1,10 +1,27 @@
-from flask import Flask
+from flask import Flask,request, render_template
+from flask_sqlalchemy import SQLAlchemy
+import bcrypt
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
   return 'Hi'
 
-if __name__ == '__main__':
+@app.route('/register', methods=['GET','POST'])
+def register():
+  if request.method=='POST':
+    #handling request
+    pass
+  return render_template('register.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+  if request.method=='POST':
+    pass
+  return render_template('login.html')
+
+
+if __name__ == '__main__':  
   app.run(debug=True)
