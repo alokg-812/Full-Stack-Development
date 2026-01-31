afeffc9dfa4199fedcdfac874365f5eefa51b8b6
alokg-812 <alokji812@gmail.com>
2026-01-31 15:10:26 +0530

    updated login functionality

--- Backend/frameworks/flaskUserAuthentication/app.py
+++ Backend/frameworks/flaskUserAuthentication/app.py
@@ -1,10 +1,11 @@
-from flask import Flask,request, render_template
+from flask import Flask,request, render_template, redirect, session
 from flask_sqlalchemy import SQLAlchemy
 import bcrypt
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db = SQLAlchemy(app)
+app.secret_key = 'secret_key'
 
 class User(db.Model):
   id=db.Column(db.Integer, primary_key=True)
@@ -12,13 +13,13 @@ class User(db.Model):
   email=db.Column(db.String(100), unique=True)
   password=db.Column(db.String(100))
 
-  def __inti__(self,email,password,name):
+  def __init__(self,name,email,password):
     self.name=name
     self.email=email
-    self.password=bcrypt.hash(password.encode('utf=8'), bcrypt.gensalt()).decode('utf-8')
+    self.password=bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
   
   def check_password(self,password):
-    return bcrypt.checkpw(password.encode('utf-8'), self.password.encode('utf-8'))
+    return bcrypt.checkpw(password.encode('utf-8'),self.password.encode('utf-8'))
 
 with app.app_context():
   db.create_all()
@@ -31,15 +32,39 @@ def index():
 def register():
   if request.method=='POST':
     #handling request
-    pass
+    name=request.form['name']
+    email=request.form['email']
+    password=request.form['password']
+
+    new_user=User(name=name, email=email, password=password)
+    db.session.add(new_user)
+    db.session.commit()
+    return redirect('/login')
+  
   return render_template('register.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
   if request.method=='POST':
-    pass
+    email=request.form['email']
+    password=request.form['password']
+
+    #filtering the email from the database email
+    user=User.query.filter_by(email=email).first()
+
+    if user and user.check_password(password):
+      session['email']=user.email
+      return redirect('/dashboard')
+    else:
+      return render_template('login.html', error='Invalid User')
+
   return render_template('login.html')
 
+@app.route('/dashboard', methods=['GET', 'POST'])
+def dashboard():
+  if session['email']:
+    user = User.query.filter_by(email=session['email']).first()
+  return render_template('dashboard.html', user=user)
 
 if __name__ == '__main__':  
   app.run(debug=True)
