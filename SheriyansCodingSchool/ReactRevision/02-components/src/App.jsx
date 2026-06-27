5dc9ce370550a5974cf3f4e874b8f05f95f44b51
alokg-812 <alokji812@gmail.com>
2026-06-27 20:20:02 +0530

    components of react

---
+++ SheriyansCodingSchool/ReactRevision/02-components/src/App.jsx
@@ -0,0 +1,18 @@
+import Card from "./components/Card";
+
+const App=()=>{
+  return(
+  <div>
+    <Card />
+    <Card />
+    <Card />
+
+    <div className="card">
+      <h2>Alok Gupta</h2>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odio deserunt non delectus esse modi aliquid.</p>
+    </div>
+  </div>
+  
+)}
+
+export default App;
